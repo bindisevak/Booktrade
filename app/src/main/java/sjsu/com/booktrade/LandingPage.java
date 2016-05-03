465ba490f230b407051abd0200804107f5a28ebe
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2016-05-02 17:31:12 -0700

    Added Book Details Page and Payment

--- app/src/main/java/sjsu/com/booktrade/LandingPage.java
+++ app/src/main/java/sjsu/com/booktrade/LandingPage.java
@@ -1,5 +1,6 @@
 package sjsu.com.booktrade;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -15,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LandingPage extends AppCompatActivity
